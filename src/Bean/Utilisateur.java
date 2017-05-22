7f82726da653b68e1e3ddc42a7ffe81c1684cb68
Valodia Tsacanias <valodia.tsacanias@gmail.com>
2017-05-22 14:47:47 +0200

    bean utilisateur

--- src/Bean/Utilisateur.java
+++ src/Bean/Utilisateur.java
@@ -6,5 +6,61 @@ package Bean;
 public class Utilisateur {
 
     private Integer id;
+    private String email;
+    private String password;
+    private String nom ;
 
+
+    //Constructeurs
+    public Utilisateur() {
+    }
+
+    public Utilisateur(Integer id, String email, String password, String nom) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nom = nom;
+    }
+    //Getters & Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    //ToString
+    @Override
+    public String toString() {
+        return "Utilisateur{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
 }
